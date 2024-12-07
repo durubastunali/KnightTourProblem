11c7530d815b78c070a8f0aa133e3ae1e9858852
Duru Baştunalı <durubastunali@gmail.com>
2024-12-08 01:43:31 +0300

    Each node holds less space

--- src/knight_tour_problem/Node.java
+++ src/knight_tour_problem/Node.java
@@ -3,8 +3,8 @@ package knight_tour_problem;
 public class Node {
     public int locationY;
     public int locationX;
-    public Node parent;
     public int depth;
+    public Node parent;
 
     public Node(Node parent, int locationX, int locationY, int depth) {
         this.parent = parent;
