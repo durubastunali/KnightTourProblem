527b89418076f2a678e2c0857a6ba5913757027e
Duru Baştunalı <durubastunali@gmail.com>
2024-12-13 18:57:16 +0300

    Comments added for Main and Node classes

--- src/knight_tour_problem/Main.java
+++ src/knight_tour_problem/Main.java
@@ -1,4 +1,6 @@
+// The Knight's Tour Problem: 150120042, 150120053, 150120075
 package knight_tour_problem;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,40 +15,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Size of the board (n): ");
+        System.out.print("Size of the board (n): "); // Take the size of the board as input from the user
         int n = scanner.nextInt();
 
         scanner.nextLine();
 
-        System.out.print("Search method (a-d): ");
+        System.out.print("Search method (a-d): "); // Take the search method as input from the user
         char searchMethod = scanner.nextLine().charAt(0);
 
-        System.out.print("Time limit (t): ");
+        System.out.print("Time limit (t): "); // Take the time limit as input from the user
         int t = scanner.nextInt();
 
         Search search = new Search(n);
 
-        timeLimit = (long) t * 60 * 1000;
-        startTime = System.currentTimeMillis();
+        timeLimit = (long) t * 60 * 1000; // Set the time limit in milliseconds
+        startTime = System.currentTimeMillis(); // Set the starting time of the program
 
-        try {
+        try { // Only search for a solution starting from the 1-1 tile
             search.treeSearch(new Node(null, 1, 1, 1), searchMethod);
-            if (search.solution != null) {
+            if (search.solution != null) { // A solution node is set, therefore a solution is found
                 printSolution(search.solution);
-            } else if (search.timeLimitPassed) {
+            } else if (search.timeLimitPassed) { //
                 System.out.println("Timeout.");
-            } else {
+            } else { // A solution node is not set, remaining null. Therefore, no solution found
                 System.out.println("No solution exists.");
             }
-        } catch (OutOfMemoryError e) {
+        } catch (OutOfMemoryError e) { //Catch possible errors
             System.out.println("Out of memory.");
         } catch (StackOverflowError e) {
             System.out.println("Stack overflow.");
         }
 
-        long endTime = System.currentTimeMillis();
-        long runTime = endTime - startTime;
+        long endTime = System.currentTimeMillis(); // Set the ending time of the program
+        long runTime = endTime - startTime; // Calculate the runtime of the program in milliseconds
 
+        //Format the runtime into mm:ss:SSS
         long minutes = runTime / (60 * 1000);
         long seconds = (runTime % (60 * 1000)) / 1000;
         long milliseconds = runTime % 1000;
@@ -55,9 +58,11 @@ public class Main {
         System.out.println("Number of nodes expanded: " + search.numberOfNodesExpanded);
     }
 
-    private static void printSolution(Node node)  {
+    private static void printSolution(Node node)  { //Print the solution path
         System.out.println("A solution found.");
         try {
+            // Output is written to a txt file.
+            // This file is used as an input for the Python program to visualize the solution path.
             File file = new File("src\\knight_tour_problem\\output.txt");
             writer = new FileWriter(file, false);
             findPath(node);
@@ -67,6 +72,7 @@ public class Main {
         }
     }
 
+    //Printing is done in recursive, so the path can be written from up to down, starting from the tile 1-1.
     private static void findPath(Node node) throws IOException {
         if (node.parent != null) {
             findPath(node.parent);
--- src/knight_tour_problem/Node.java
+++ src/knight_tour_problem/Node.java
@@ -1,10 +1,11 @@
+// The Knight's Tour Problem: 150120042, 150120053, 150120075
 package knight_tour_problem;
 
 public class Node {
-    public int locationY;
-    public int locationX;
-    public int depth;
-    public Node parent;
+    public int locationX; // X location of the Node
+    public int locationY; // Y location of the Node
+    public int depth; // Depth of the Node
+    public Node parent; // Parent of the Node
 
     public Node(Node parent, int locationX, int locationY, int depth) {
         this.parent = parent;
